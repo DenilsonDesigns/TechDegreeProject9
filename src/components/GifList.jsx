3457234561f351bd8a0e92c414095f634f141924
DenilsonDesigns <daniellanguilller@gmail.com>
2018-09-08 22:54:39 +1000

    added giflist

--- src/components/GifList.jsx
+++ src/components/GifList.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Gif from "./Gif";
 
 const GifList = props => {
+  const results = props.data;
+  let gifs = results.map(gif => <Gif />);
   return (
     <ul>
       <Gif />
